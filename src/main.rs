2926e82b8b3ecf2b663f368c66a9a0c5c3ef875f
Pete Windle <git@pete23.com>
2020-02-08 15:29:11 +0000

    first guesser

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::cmp::Ordering;
+use std::io;
+use std::io::Write;
+use rand::Rng;
+
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
+    println!("SEKRIT IS {}", secret_number);
+
+    loop {
+	print!("Please input your guess: ");
+	io::stdout().flush().expect("not flushed i guess");
+	
+	let mut guess = String::new();
+	
+	io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+	let guess: u32 = guess.trim().parse().expect("Please type a number!");
+	
+	
+	println!("You guessed: {}", guess);
+	
+	match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+		println!("YAY YAY YAY YOU FOUNDED IT");
+		break;
+	    }
+	}
+    }
+}
