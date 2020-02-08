4ad97414c2a6ae474d8ff3d9953cd6d804fc3add
Pete Windle <git@pete23.com>
2020-02-08 15:29:56 +0000

    make it slightly less challenging

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
-    println!("SEKRIT IS {}", secret_number);
 
     loop {
 	print!("Please input your guess: ");
@@ -19,10 +18,7 @@ fn main() {
 	io::stdin().read_line(&mut guess).expect("Failed to read line");
 
 	let guess: u32 = guess.trim().parse().expect("Please type a number!");
-	
-	
-	println!("You guessed: {}", guess);
-	
+
 	match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
